627d7ca48d4e897430c9ee89feae39e7666ef5b9
regantek <reganbarua2015@gmail.com>
2020-12-31 20:55:38 -0800

    Task finished

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 import "./style.css";
-import Clock from "./Clock";
+
+function SayHello() {
+  return <h1>Hello</h1>;
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <Clock />
+        <SayHello />
       </div>
     );
   }
